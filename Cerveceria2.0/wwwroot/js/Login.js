c96dd0a211a22369ac51aca8e6cd3a4ab5367e44
snadyezhda <nadiahavlicek@gmail.com>
2021-03-10 16:48:51 -0300

    Agregar archivos de proyecto.

---
+++ Cerveceria2.0/wwwroot/js/Login.js
@@ -0,0 +1,30 @@
+﻿function Login() {
+    var email = $("#InputEmail").val();
+    var password = $("#InputPassword").val();
+
+
+    ///VAlidaciones si email es null o password
+    var user = {
+        email: email,
+        telefono:
+        password: password
+    }
+
+
+    var settings = {
+        "url": "https://localhost:44338/Account/login", //AGREGAR LA URL DEL LOGIN DE ACA
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify(user),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    }).fail(function (error) {
+        console.log(response);
+    });
+
+}
